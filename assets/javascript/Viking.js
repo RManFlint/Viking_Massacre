e15f8cca7b43be2b45a4db950f1e58a167a4e350
Jim Holmberg <hasenpfad1@gmail.com>
2019-03-17 15:54:47 -0500

    Adding images with button

--- assets/javascript/Viking.js
+++ assets/javascript/Viking.js
@@ -4,14 +4,23 @@ $(document).ready(function() {
 return document.getElementById(id); 
 };*/
 
-var lego; /* = "<img src = '../../assets/images/lego.jpg' alt='Lego Viking'>";
-*/
-var leif;
-var thorvald;
-var ragnar;
+var lego = "<button class = 'character' id = 'charLego'><img src = './assets/images/lego.jpg' alt='Lego Viking'></button>";
+var leif = "<button class = 'character' id = 'charLeif'><img src = './assets/images/leif.jpg' alt='Leif Viking'>";
+var thorvald = "<button class = 'character' id = 'charThorvald'><img src = './assets/images/thorvald.jpg' alt='Thorvald Viking'></button>";
+var ragnar = "<button class = 'character' id = 'charRagnar'><img src = './assets/images/ragnar.jpg' alt='Ragnar Viking'></button>";
 
 function bullpen() {
-	$("#bullpen1").html("<img src='../../assets/images/lego.jpg' alt='Lego Viking'>");
+	$("#bullpen1").html(lego);
+	$("#bullpen2").html(leif);
+	$("#bullpen3").html(thorvald);
+	$("#bullpen4").html(ragnar);
 }
 bullpen();
+
+$(".character").click(function() {
+	$("#chosenViking").html(parent(this).html);
+
+});
+
+
 })
